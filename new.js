53281633bffc6e42184b664f7f4906048e469005
tonye-d <itztonye@gmail.com>
2017-10-21 19:29:18 +0100

    something in js

--- new.js
+++ new.js
@@ -1,23 +1,23 @@
 $(function() {
-  workbelt();
-  workload();
+    workbelt();
+    workload();
 });
 
-  // Get started!
+// Get started!
 
 function workbelt() {
 
     $('.view').click(function() {
-      $('.work-belt').css('left', '-100%');
-      $('.work-container').show();
-      $('.card').hide(800);
+        $('.work-belt').css('left', '-100%');
+        $('.work-container').show();
+        $('.card').hide(800);
     });
-    
+
 
     $('.fa-arrow-circle-left').click(function() {
-      $('.work-belt').css('left', '0%');
-      $('.work-container').hide(800);
-      $('.card').show();
+        $('.work-belt').css('left', '0%');
+        $('.work-container').hide(800);
+        $('.card').show();
     });
 }
 
@@ -25,10 +25,10 @@ function workload() {
     $.ajaxSetup({ cache: true });
 
     $('.view').click(function() {
-      var spinner = '<div class="loader">Loading...</div>',
-          newHTML = 'student.html';
-      $('.project-load').html(spinner).load(newHTML);
+        var spinner = '<div class="loader">Loading...</div>',
+            newHTML = 'student.html';
+        $('.project-load').html(spinner).load(newHTML);
     });
 
-   
+
 }
